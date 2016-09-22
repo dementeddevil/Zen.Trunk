34ee9f1e51a2818d72c055998a0e0c6964ea70dc
DementedDevil <zen_global@hotmail.com>
2016-09-22 06:55:08 +0100

    Service updates - started work on fallback configuration system based on registry keys...

--- src/Zen.Trunk.StorageEngine.Service/TrunkStorageEngineService.cs
+++ src/Zen.Trunk.StorageEngine.Service/TrunkStorageEngineService.cs
@@ -1,4 +1,8 @@
-﻿namespace Zen.Trunk.StorageEngine.Service
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Win32;
+
+namespace Zen.Trunk.StorageEngine.Service
 {
     public partial class TrunkStorageEngineService : InstanceServiceBase
     {
@@ -9,10 +13,122 @@
 
         protected override void OnStart(string[] args)
         {
+            // TODO: Initialise our custom configuration system (registry based)
+
+            // Initialise logging framework
+
         }
 
         protected override void OnStop()
         {
         }
+    }
+
+    public class TrunkConfigurationSection
+    {
+        private readonly IDictionary<string, TrunkConfigurationSection> _subSections =
+            new Dictionary<string, TrunkConfigurationSection>(StringComparer.OrdinalIgnoreCase);
+        private RegistryKey _instanceKey;
+        private RegistryKey _globalKey;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TrunkConfigurationSection"/> class.
+        /// </summary>
+        /// <param name="instance">The instance.</param>
+        /// <param name="global">The global.</param>
+        public TrunkConfigurationSection(RegistryKey instance, RegistryKey global)
+        {
+            _instanceKey = instance;
+            _globalKey = global;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="TrunkConfigurationSection"/> with the specified sub section.
+        /// </summary>
+        /// <value>
+        /// The <see cref="TrunkConfigurationSection"/>.
+        /// </value>
+        /// <param name="subSection">The sub section.</param>
+        /// <returns></returns>
+        public TrunkConfigurationSection this[string subSection]
+        {
+            get
+            {
+                TrunkConfigurationSection section;
+                if (!_subSections.TryGetValue(subSection, out section))
+                {
+                    // ReSharper disable once JoinDeclarationAndInitializer
+                    RegistryKey subInstanceKey, subGlobalKey = null;
+                    subInstanceKey = _instanceKey.OpenSubKey(subSection);
+                    try
+                    {
+                        subGlobalKey = _globalKey?.OpenSubKey(subSection);
+                    }
+                    catch
+                    {
+                    }
+                    section = new TrunkConfigurationSection(subInstanceKey, subGlobalKey);
+                    _subSections.Add(subSection, section);
+                }
+                return section;
+            }
+        }
+
+        /// <summary>
+        /// Gets the value for the configuration entry.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <param name="keyName">Name of the configuration element.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <param name="allowFallback">
+        /// if set to <c>true</c> then fallback to reading from global settings
+        /// if instance value doesn't exist; otherwise <c>false</c>.
+        /// </param>
+        /// <returns></returns>
+        public TResult GetValue<TResult>(
+            string keyName,
+            TResult defaultValue = default(TResult),
+            bool allowFallback = true)
+        {
+            var value = _instanceKey.GetValue(keyName);
+            if (value == null && allowFallback && _globalKey != null)
+            {
+                value = _globalKey.GetValue(keyName);
+            }
+            if (value == null)
+            {
+                value = defaultValue;
+            }
+            return (TResult)value;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TrunkConfigurationManager
+    {
+        private const string GlobalRootRegistryKeyPathBase = "Software\\Zen Design Software\\Trunk";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TrunkConfigurationManager"/> class.
+        /// </summary>
+        /// <param name="serviceName">Name of the service.</param>
+        public TrunkConfigurationManager(string serviceName)
+        {
+            var globalMachineKeyRoot = Registry.LocalMachine.OpenSubKey(
+                GlobalRootRegistryKeyPathBase + "\\Global");
+            var instanceMachineKeyRoot = Registry.LocalMachine.OpenSubKey(
+                GlobalRootRegistryKeyPathBase + "\\Instances\\" + serviceName);
+            Root = new TrunkConfigurationSection(instanceMachineKeyRoot, globalMachineKeyRoot);
+        }
+
+        /// <summary>
+        /// Gets the root.
+        /// </summary>
+        /// <value>
+        /// The root.
+        /// </value>
+        public TrunkConfigurationSection Root { get; }
     }
 }
