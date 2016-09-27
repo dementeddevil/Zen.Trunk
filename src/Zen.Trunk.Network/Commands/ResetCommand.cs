eb9c9c8e0ded86ceafe1bf4a2439e67268cdc130
DementedDevil <zen_global@hotmail.com>
2016-09-27 19:32:56 +0100

    Command support

---
+++ src/Zen.Trunk.Network/Commands/ResetCommand.cs
@@ -0,0 +1,15 @@
+﻿using SuperSocket.SocketBase.Command;
+using SuperSocket.SocketBase.Protocol;
+
+namespace Zen.Trunk.Network.Commands
+{
+    public class ResetCommand : CommandBase<TrunkSocketAppSession, BinaryRequestInfo>
+    {
+        public override string Name => "RSET";
+
+        public override async void ExecuteCommand(TrunkSocketAppSession session, BinaryRequestInfo requestInfo)
+        {
+            
+        }
+    }
+}
