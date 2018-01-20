9f3281623a09dbef285575e842b4e53029b42346
Adrian Lewis <zen_global@hotmail.com>
2018-01-20 16:20:55 +0000

    Reduced folder nesting

--- src/Zen.Trunk.StorageEngine/CoreException.cs
+++ src/Zen.Trunk.StorageEngine/CoreException.cs
@@ -2,7 +2,7 @@ using System;
 using System.Runtime.Serialization;
 using System.Security.Permissions;
 
-namespace Zen.Trunk
+namespace Zen.Trunk.Storage
 {
 	/// <summary>
 	/// The <b>CoreException</b> class is the base class for all exception
